8cedbe3eae4492f4e508c33653730494a813bd53
Madhav Patel <madhavpatel@Madhavs-MacBook-Pro.local>
2025-09-18 18:09:49 -0400

    Update TestGarageDoors and TestObservers

--- src/adapter/TestGarageDoors.java
+++ src/adapter/TestGarageDoors.java
@@ -3,8 +3,13 @@ package Adapter;
 public class TestGarageDoors {
     public static void main(String[] args) {
         GarageDoorAdapter my_door = new GarageDoorAdapter("17");
+        PremiumGarageDoor door = new PremiumGarageDoor("premium");
+        
+        my_door.open(); // no speed (for basic door)
+        my_door.close(); // no speed (for basic door)
 
-        my_door.open();
-        my_door.close();
+        door.moveUp(20.0); // speed given (for premium door) ; opening
+        door.moveDown(15.0); // speed given (for premium door) ; closing    
+
     }
 }
--- src/observer/TestObservers.java
+++ src/observer/TestObservers.java
@@ -2,10 +2,25 @@ package observer;
 
 public class TestObservers {
     public static void main(String[] args) {
+        // create stores
         Store walmart = new Store("Walmart", 10.0f);
         Store costco = new Store("Costco", 15.0f);
 
+        // create customers and register them to their favourite stores
         Customer customer1 = new Customer("John Doe", walmart);
         Customer customer2 = new Customer("Jane Doe", costco);
+
+        customer1.register(costco);
+        customer2.register(walmart);
+
+        // change discounts and see the notifications
+        walmart.setDiscount("new years", 20.0f);
+        costco.setDiscount("black friday", 25.0f);
+
+        // unregister customer1 from costco and change discount again
+        customer1.unregister(costco);
+        
+        // change discounts and see the notifications
+        walmart.setDiscount("easter", 30.0f);
     }
 }
